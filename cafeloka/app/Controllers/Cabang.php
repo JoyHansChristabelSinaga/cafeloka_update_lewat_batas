85e731694d1a38f740f6d6d9810537c8a9481fa4
JoyHansChristabelSinaga <83484843+JoyHansChristabelSinaga@users.noreply.github.com>
2022-11-24 07:43:57 +0700

    12hfytfykufrilyux

---
+++ cafeloka/app/Controllers/Cabang.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\Data;
+use App\Models\Daerah;
+
+class Cabang extends BaseController
+{
+    function __construct()
+    {
+        $this->daerah = new Daerah();
+        $this->data = new Data();
+    }
+
+    public function index($id)
+    {
+        $daerah = $this->data->where('id_daerah',$id)->findAll();
+
+        $_data['data'] = $daerah;
+
+        return view('data/cafeSetiapCabang', $_data);
+    }
+    
+    public function index2($id)
+    {
+        $daerah = $this->data->where('id_daerah',$id)->findAll();
+
+        $_data['data'] = $daerah;
+
+        return view('data/cafeSetiapCabang_user', $_data);
+    }
+}
